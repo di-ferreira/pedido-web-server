d963bfe24e323f2c640ead3b1a7317d3e34c69c1
Diego Ferreira <diferreira.program@gmail.com>
2026-04-13 14:00:20 -0300

    up

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,5 @@
 import { compareSync, genSaltSync, hashSync } from 'bcryptjs';
-import { type ClassValue, clsx } from 'clsx';
+import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
