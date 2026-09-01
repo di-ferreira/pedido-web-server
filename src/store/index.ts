78bda94ab539fddc7ff91babbadf7e3eb5bc7c85
Diego Ferreira <diferreira.program@gmail.com>
2026-09-01 17:25:24 -0300

    chore: remover código morto (fetchClient, modalStore, DataTable TanStack, 8 deps)
    - Remove src/lib/fetchClient/ (legado, não usado)
    - Remove src/store/modalStore.ts (não usado)
    - Remove src/components/DataTable/ (TanStack, não usado)
    - Remove deps: @nextui-org/modal, @tanstack/react-table, axios, cookies-next,
      js-cookie, lucide-react, recharts, @types/axios, @types/js-cookie
    - Bundle reduzido (~100 bytes por página)

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,4 @@
 export { default as useBudget } from './BudgetStore';
 export { default as useUser } from './UserStore';
-export { default as useModal } from './modalStore';
 export { default as useProduct } from './useProductStore';
 
